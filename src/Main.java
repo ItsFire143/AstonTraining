e5e393daa9c20b90ebcf295aebfd4d4fac02b260
Big Brother <112923603+ItsFire143@users.noreply.github.com>
2025-05-30 14:18:30 +0300

    lesson_2 correction

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
    public static void main(String[] args) {
 
-// исключения в массивах
-         String[][] array = {
-                 {"1", "2", "3", "15"},
-                 {"5", "3", "Шашлык", "8"},
-                 {"9", "10", "11", "12"},
-                 {"13", "Tiger", "15", "16"}
-         };
+       Park park = new Park();
 
-         try {
-             int result = ArrayChecker.TransAndSumArray(array);
-             System.out.println("Сумма массива " + result);
-         } catch (MyArraySizeException e) {
-             System.err.println("Ошибка размара массива " + e.getMessage());
-         } catch (ArrayDataException e) {
-             System.err.println("Ошибка данных " + e.getMessage());
-             System.out.println("Сумма корректных значений " + e.getCellErrSum());
-         }
+       park.addAttraction("Пивной ларек", "10:00-22:00", 75.59);
 
-// исклюения в индексе массива
-       try {
-           ArrayIndexCatcher.accessInvalidIndex();
-       } catch (ArrayIndexOutOfBoundsException e) {
-           System.out.println(" ");
+       List<Park.Attraction> attract = new ArrayList<>();
+       attract.add(park.new Attraction("Ларек с шавермой", "12:00-20:00", 156.0));
+       attract.add(park.new Attraction("Туалет", "00:00-00:01", 100.50));
+
+       park.addAttractions(attract);
+
+       for (Park.Attraction attraction : park.getAttractions()) {
+           System.out.println(attraction);
        }
-
    }
 }
