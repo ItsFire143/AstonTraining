8777266b50fb3cff279ecb163a9e8d59af95b67e
Roman Pavlov <112923603+ItsFire143@users.noreply.github.com>
2025-05-30 14:26:06 +0300

    correction Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
-import java.util.stream.StreamSupport;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-    }
+   public static void main(String[] args) {
+
+       Park park = new Park();
+
+       park.addAttraction("Пивной ларек", "10:00-22:00", 75.59);
+
+       List<Park.Attraction> attract = new ArrayList<>();
+       attract.add(park.new Attraction("Ларек с шавермой", "12:00-20:00", 156.0));
+       attract.add(park.new Attraction("Туалет", "00:00-00:01", 100.50));
+
+       park.addAttractions(attract);
+
+       for (Park.Attraction attraction : park.getAttractions()) {
+           System.out.println(attraction);
+       }
+   }
 }
