c7f0aed10a7d365217f433d4fd5edae4bb5a4ec8
Roman Pavlov <112923603+ItsFire143@users.noreply.github.com>
2025-06-06 15:03:10 +0300

    Delete src/main/java/org/example/TriangleArea.java

--- src/main/java/org/example/TriangleArea.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class TriangleArea {
-    public double calculateArea (double a, double b, double c) {
-        double s = (a + b + c) / 2;
-        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
-    }
-}
